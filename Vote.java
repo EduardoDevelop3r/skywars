fc91e2c6d3b66641fdeccd9c72e70fa22e8367f9
iambestfy <37350082+EduardoDevelop3r@users.noreply.github.com>
2018-04-06 23:53:01 -0300

    Create Vote.java

---
+++ Vote.java
@@ -0,0 +1,8 @@
+package com.bestfyplugner.skywars.manager;
+
+public class Vote {
+
+	private boolean rain,doubleJump,sponge,teleportBow;
+	private long time;
+	private Storage storage = Skywars.STORAGE_BASIC;
+}
